6ca29c670aeeaaf7c7b0fcae4fc683feade83300
monicalopezgris <monicalopezgris@gmail.com>
2020-01-23 19:09:39 +0100

    ADD logo & style to login /signin page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import { BrowserRouter as Router, Switch } from 'react-router-dom';
+import styled from 'styled-components';
 import AuthContext from './lib/AuthCtx';
 import SecureRoute from './components/SecureRoute';
 import AnonRoute from './components/AnonRoute';
@@ -9,17 +10,27 @@ import Header from './components/Header';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 
+const Wrapper = styled.div`
+  margin:0;
+  padding:0;
+  width: 300px;
+  height: 530px;
+  background-color: #096c77;
+`;
+
 function App() {
   return (
     <AuthContext>
-      <Router>
-        <Header />
-        <Switch>
-          <AnonRoute exact path="/login" component={Login} />
-          <SecureRoute exact path="/" component={MessageBoard} />
-          <AnonRoute path="/signup" component={Signup} />
-        </Switch>
-      </Router>
+      <Wrapper>
+        <Router>
+          <Header />
+          <Switch>
+            <AnonRoute exact path="/login" component={Login} />
+            <SecureRoute exact path="/" component={MessageBoard} />
+            <AnonRoute path="/signup" component={Signup} />
+          </Switch>
+        </Router>
+      </Wrapper>
     </AuthContext>
   );
 }
